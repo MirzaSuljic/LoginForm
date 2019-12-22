f0ac3da7f2c4926cf19dd492465e833d62ab08f0
Mirza Suljić <mirza.suljic@gmail.com>
2019-12-22 18:41:37 +0100

    loginForm

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+
+@Injectable({ providedIn: 'root' })
+export class AuthenticationService {
+    private currentUserSubject: BehaviorSubject<any>;
+    public currentUser: Observable<any>;
+
+    constructor(private http: HttpClient) {
+        this.currentUserSubject = new BehaviorSubject<any>(JSON.parse(localStorage.getItem('currentUser')));
+        this.currentUser = this.currentUserSubject.asObservable();
+    }
+
+    public get currentUserValue() {
+        return this.currentUserSubject.value;
+    }
+
+    // login(username, password) {
+    //     return new Promise((resolve, reject) => {
+    //         if (username === '' && password == '') {
+    //             localStorage.setItem('currentUser', JSON.stringify({ username, password }));
+    //             this.currentUserSubject.next({ username, password });
+    //             resolve();
+    //         }
+    //         else {
+    //             reject();
+    //         }
+    //     });
+    // }
+    login(username: string, password: string) {
+        return this.http.post<any>(`${environment.apiUrl}/users/authenticate`, { username, password })
+            .pipe(map(user => {
+                // login successful if there's a jwt token in the response
+                if (user && user.token) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(user));
+                    this.currentUserSubject.next(user);
+                }
+
+                return user;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage and set current user to null
+        localStorage.removeItem('currentUser');
+        this.currentUserSubject.next(null);
+    }
+}
